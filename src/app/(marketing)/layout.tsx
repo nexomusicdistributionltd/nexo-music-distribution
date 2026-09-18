6d69af469b14587ae309db1b59dd9d7b81d522de
nexomusicdistributionltd <nexomusicdistribution@gmail.com>
2026-09-18 10:46:13 -0700

    Enable CMS realtime refresh across public website

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { PublicEffects } from "@/components/public/PublicEffects";
+import { PublicCatalogRealtime } from "@/components/website/PublicCatalogRealtime";
 import { publicDisplay, publicSans } from "@/components/public/fonts";
 import "./public.css";
 
@@ -12,6 +13,7 @@ export default function MarketingLayout({
   return (
     <div className={`${publicDisplay.variable} ${publicSans.variable} nexo-public flex min-h-screen flex-col`}>
       <PublicEffects />
+      <PublicCatalogRealtime />
       <Navbar />
       <main className="pub-shell-main">{children}</main>
       <Footer />
